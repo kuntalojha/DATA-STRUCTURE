db995325d14f73ed31939a1858c10b5829109b9c
Kuntal Ojha <kuntalojha2021@gmail.com>
2025-06-15 15:28:45 +0530

    feat: add Experiment 7 with circular queue implementation

--- docs/.vitepress/config.js
+++ docs/.vitepress/config.js
@@ -35,7 +35,7 @@ export default defineConfig({
           { text: 'Experiment 04', link: '/dslab/exp-4.md' },
           { text: 'Experiment 05', link: '/dslab/exp-5.md' },
           { text: 'Experiment 06', link: '/dslab/exp-6.md' },
-          // { text: 'Experiment 07', link: '/dslab/exp-7.md' },
+          { text: 'Experiment 07', link: '/dslab/exp-7.md' },
           // { text: 'Experiment 08', link: '/dslab/exp-8.md' },
           // { text: 'Experiment 09', link: '/dslab/exp-9.md' },
           // { text: 'Experiment 10', link: '/dslab/exp-10.md' },
@@ -50,7 +50,7 @@ export default defineConfig({
       { icon: 'linkedin', link: 'https://www.linkedin.com/in/mrkuntalojha/' },
       { icon: 'twitter', link: 'https://x.com/kuntalojha' },
       { icon: 'youtube', link: 'https://www.youtube.com/@kuntalojhadev' },
-      { icon: 'youtube', link: 'https://www.youtube.com/@kuntalojha' },
+      //{ icon: 'youtube', link: 'https://www.youtube.com/@kuntalojha' },
     ],
   },
 });
