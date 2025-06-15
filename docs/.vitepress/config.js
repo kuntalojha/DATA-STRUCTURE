bf6b04cc922cccc817e0dc57bf57e938a15a317d
Kuntal Ojha <kuntalojha2021@gmail.com>
2025-06-15 20:38:55 +0530

    Refactor code structure for improved readability and maintainability

--- docs/.vitepress/config.js
+++ docs/.vitepress/config.js
@@ -24,6 +24,7 @@ export default defineConfig({
           { text: 'M2 Unit 02', link: '/ds/modules/module-2/unit-2' },
           { text: 'Module 03', link: '/ds/modules/module-3/module-3' },
           { text: 'Module 04', link: '/ds/modules/module-4/module-4' },
+          { text: 'Module 05', link: '/ds/modules/module-5/module-5' },
         ],
       },
       {
@@ -37,7 +38,7 @@ export default defineConfig({
           { text: 'Experiment 06', link: '/dslab/exp-6.md' },
           { text: 'Experiment 07', link: '/dslab/exp-7.md' },
           { text: 'Experiment 08', link: '/dslab/exp-8.md' },
-          // { text: 'Experiment 09', link: '/dslab/exp-9.md' },
+          { text: 'Experiment 09', link: '/dslab/exp-9.md' },
           // { text: 'Experiment 10', link: '/dslab/exp-10.md' },
           // { text: 'Experiment 11', link: '/dslab/exp-12.md' },
           // { text: 'Experiment 13', link: '/dslab/exp-13.md' },
--- docs/dslab.md
+++ docs/dslab.md
@@ -14,3 +14,8 @@ outline: auto
 - [Experiment 06](dslab/exp-6)
 - [Experiment 07](dslab/exp-7)
 - [Experiment 08](dslab/exp-8)
+- [Experiment 09](dslab/exp-9)
+<!-- - [Experiment 10](dslab/exp-10) -->
+<!-- - [Experiment 11](dslab/exp-11) -->
+<!-- - [Experiment 12](dslab/exp-12) -->
+<!-- - [Experiment 13](dslab/exp-13) -->
