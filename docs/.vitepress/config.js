2ff8b9563edc90394438c71514faf78757e6661d
Kuntal Ojha <kuntalojha2021@gmail.com>
2025-06-18 21:53:00 +0530

    fix: correct link path for Question in navigation

--- docs/.vitepress/config.js
+++ docs/.vitepress/config.js
@@ -10,7 +10,7 @@ export default defineConfig({
     // https://vitepress.dev/reference/default-theme-config
     nav: [
       { text: 'Home', link: '/' },
-      { text: 'Question', link: 'question' },
+      { text: 'Question', link: '/question' },
     ],
 
     sidebar: [
